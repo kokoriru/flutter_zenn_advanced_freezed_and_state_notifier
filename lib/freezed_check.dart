f44bef9b7327613bd98c194cd08c9c2c41e182b8
kokoriru <yuka@ryukalice.com>
2021-11-23 18:12:23 +0900

    User モデルの動作確認用メソッド追加

---
+++ lib/freezed_check.dart
@@ -0,0 +1,31 @@
+import 'user.dart';
+
+void func() {
+  // ユーザーを3つ作成する。user1 と user3 は同じ内容。
+  User user1 = User('ogiyu1', 1);
+  User user2 = User('ogiyu2', 2);
+  User user3 = User('ogiyu1', 1);
+
+  // 表示
+  print('check1');
+  print(user1);
+
+  // 比較
+  print('check2');
+  if (user1 == user2) print('user1 と user2 は同じ人です');
+  if (user1 == user3) print('user1 と user3 は同じ人です');
+
+  // コピーをして新しいインスタンスを作成
+  print('check3');
+  User user4 = user1.copyWith(name: 'ogiyu1_copy');
+  print(user1);
+  print(user4);
+
+  // 参照そのものを変えるのは OK
+  print('check4');
+  user2 = user3;
+  print(user2);
+
+  // immutable を破壊するので、以下のような使い方は NG
+  // user1.name = 'unknown';
+}
